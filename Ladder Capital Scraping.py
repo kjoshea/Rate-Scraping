08bb90be975c02d5718e9453e33826c5bcf6e537
kjoshea <osheak7@yahoo.com>
2020-02-09 12:43:14 -0800

    Send 5 Year Swap Rate from Gmail Account using SMTP.

--- Ladder Capital Scraping.py
+++ Ladder Capital Scraping.py
@@ -1,6 +1,12 @@
+# Import Libraries for Accessing Ladder Capital Website
 from requests_html import HTML, HTMLSession
 import pandas as pd
 
+# Import Libraries for Emailing from GMAIL
+import os
+import smtplib
+
+# -------------------------------------------
 # Access Ladder Capital's Front Page Website
 session = HTMLSession()
 r = session.get('https://laddercapital.com')
@@ -14,12 +20,30 @@ table_html = table.html
 a = pd.read_html(table_html,index_col=0)
 table_pd = a[0]
 
-# Store the 5 Year Swap
-five = table_pd.iat[0,2]
+# Store the 5 Year Swap (Only First 6 Characters to Avoid the Arrow Symbol)
+five = table_pd.iat[0,2][:6]
 
 # Print the Rate Table and 5 Year Swap
 print(table_pd)
 print(five)
 
+# ---------------------------------------------------------------
+# Pull Gmail Login Credentials from Local Environment Variables
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
+# Send Email Using Gmail
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
 
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    # Set Email Subject and Body Content and Save it in a Message Variable
+    subject = 'US Rates'
+    body = f'The 5 Year Swap is {five}.'
+    msg = f'Subject: {subject}\n\n{body}'
+
+    # Send Email Message
+    smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg.encode('utf8'))
